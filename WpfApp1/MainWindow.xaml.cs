a82d791e3a7473eb52e983f83a89518dbfe07eee
Loic PASTOR <lpastor812@gmail.com>
2020-02-29 18:31:46 +0100

    Point affiché sur la carte + début création paramètres

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -32,30 +32,61 @@ namespace WpfApp1
         private void testClic(object sender, MouseEventArgs e)
         {
             MouseEventArgs me = (MouseEventArgs)e;
+            Rectangle rect = new Rectangle { Width = 5, Height = 5, Fill = Brushes.Black };
+            TextBlock villInfo = new TextBlock();
+            villInfo.Text = "Ville " + (items.Count + 1);
+            testCanvas.Children.Add(villInfo);
+            testCanvas.Children.Add(rect);
+
+
             var test = me.GetPosition(testCanvas);
             Ville newVille = new Ville((items.Count + 1), "Ville " + (items.Count + 1), test.X, test.Y);
+            Canvas.SetLeft(rect, test.X);
+            Canvas.SetTop(rect, test.Y);
+            Canvas.SetLeft(villInfo, test.X);
+            Canvas.SetTop(villInfo, test.Y-15);
             items.Add(newVille);
 
             listVilles.Items.Add(newVille);
         }
-/*
-        private void saveVilles(object sender, RoutedEventArgs e)
-        {
-            Ville ville = new Ville()
+
+        private void launchAlgorithm(object sender, RoutedEventArgs e) {
+
+            int limit = 1;
+
+            int cheminsNumber = Int32.Parse(nbChemins.Text);
+        
+           List<Chemin> Chemins = Chemin.setFirstGen(cheminsNumber, items);
+
+
+            do
             {
-                Nom = NomTextBox.Text,
-                X = XTextBox.Text,
-                Y = YTextBox.Text
-
-            };
-
-            Database.GetDatabase().saveVille(ville);
-
-            
-
-            Close();
-
+                List<Chemin> elites = Chemin.Elite(Chemins, cheminsNumber / 5);
+                List<Chemin> testo = Chemin.crossover(cheminsNumber, Chemins, items);
+                List<Chemin> muty = Chemin.mutation(cheminsNumber, Chemins);
+                List<Chemin> newGen = Chemin.setNewGen(testo, muty, elites, cheminsNumber);
+                limit++;
+            }
+            while (limit < Int32.Parse(nbGenerations.Text));
         }
-        */
+        /*
+       private void saveVilles(object sender, RoutedEventArgs e)
+       {
+           Ville ville = new Ville()
+           {
+               Nom = NomTextBox.Text,
+               X = XTextBox.Text,
+               Y = YTextBox.Text
+
+           };
+
+           Database.GetDatabase().saveVille(ville);
+
+
+
+           Close();
+
+       }
+       */
     }
 }
